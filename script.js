71b099305fee74ec48389e51e00fedc486b7d262
Natalie-Cardot <nataliecardot@gmail.com>
2021-02-25 18:35:41 -0800

    Move hour hand

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
   const now = new Date();
@@ -6,6 +8,10 @@ function setDate() {
   // To turn seconds value into degrees (of full rotation around a circle), first turn into a number with a base 100 (seconds / 60 to get decimal value out of 100), then multiply by 360. Adding 90 to offset default rotation of 90deg (to make hand vertical)
   const secondsDegrees = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + 90;
+  hourHand.style.transform = `rotate(${minsDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
